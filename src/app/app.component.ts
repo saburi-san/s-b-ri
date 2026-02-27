222cd61a2b235e7fb6c051650f40d836ee798e6d
saburi-san <unashesaburi@outlook.com>
2026-02-27 08:52:44 +0200

    latest version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,36 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 })
 export class AppComponent {
   title = 'mk-i';
+
+
+  constructor(private router: Router)
+  {
+
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  shortcuts(event: any)
+  {
+
+
+    if (event.altKey && event.key == 'h')
+    {
+      this.router.navigate(['/']);
+    }
+    else if (event.altKey && event.key == 'c')
+    {
+      this.router.navigate(['/catalogue']);
+    }
+    else if (event.altKey && event.key == 'a')
+    {
+      this.router.navigate(['/about']);
+    }
+    else if (event.altKey && event.key == 'C')
+    {
+      this.router.navigate(['/contact']);
+    }
+  }
+
   @HostListener('ontouchstart', ['$event'])
   touchstart(event: any)
   {
@@ -41,7 +71,7 @@ export class AppComponent {
         if (sibling == siblings[element.cellIndex - 1] && sibling != undefined)
           sibling.style.background = '#bca58f';
         else if (sibling == siblings[element.cellIndex + 1] && sibling != undefined)
-          sibling.style.background = 'brown';
+          sibling.style.background = '#926851';
         else if (sibling != siblings[element.cellIndex] && sibling != undefined)
           sibling.style.background = 'seagreen';
 
@@ -51,7 +81,7 @@ export class AppComponent {
       if (tr[Array.prototype.indexOf.call(tr, element.parentNode) + 1] != undefined)
         tr[Array.prototype.indexOf.call(tr, element.parentNode) + 1].children[element.cellIndex].style.background = '#a9846a';
       if (tr[Array.prototype.indexOf.call(tr, element.parentNode) - 1] != undefined)
-        tr[Array.prototype.indexOf.call(tr, element.parentNode) - 1].children[element.cellIndex].style.background = '#a58fbc';
+        tr[Array.prototype.indexOf.call(tr, element.parentNode) - 1].children[element.cellIndex].style.background = '#a17a7a';
     }
 
     if (event.target.tagName == 'INPUT' )
@@ -95,7 +125,7 @@ export class AppComponent {
         if (sibling == siblings[element.cellIndex - 1] && sibling != undefined)
           sibling.style.background = '#bca58f';
         else if (sibling == siblings[element.cellIndex + 1] && sibling != undefined)
-          sibling.style.background = 'brown';
+          sibling.style.background = '#926851';
         //else if (sibling != siblings[element.cellIndex] && sibling != undefined)
           //sibling.style.background = 'seagreen';
 
@@ -105,7 +135,7 @@ export class AppComponent {
       if (tr[Array.prototype.indexOf.call(tr, element.parentNode) + 1] != undefined)
         tr[Array.prototype.indexOf.call(tr, element.parentNode) + 1].children[element.cellIndex].style.background = '#a9846a';
       if (tr[Array.prototype.indexOf.call(tr, element.parentNode) - 1] != undefined)
-        tr[Array.prototype.indexOf.call(tr, element.parentNode) - 1].children[element.cellIndex].style.background = '#a58fbc';
+        tr[Array.prototype.indexOf.call(tr, element.parentNode) - 1].children[element.cellIndex].style.background = '#a17a7a';
     }
 
 
@@ -127,7 +157,7 @@ export class AppComponent {
         if (sibling == siblings[parent.cellIndex - 1] && sibling != undefined)
           sibling.style.background = '#bca58f';
         else if (sibling == siblings[parent.cellIndex + 1] && sibling != undefined)
-          sibling.style.background = 'brown';
+          sibling.style.background = '#926851';
         //else if (sibling != siblings[parent.cellIndex] && sibling != undefined)
           //sibling.style.background = 'seagreen';
 
@@ -135,7 +165,7 @@ export class AppComponent {
       if (tr[Array.prototype.indexOf.call(tr, parent.parentNode) + 1] != undefined)
         tr[Array.prototype.indexOf.call(tr, parent.parentNode) + 1].children[parent.cellIndex].style.background = '#a9846a';
       if (tr[Array.prototype.indexOf.call(tr, parent.parentNode) - 1] != undefined)
-        tr[Array.prototype.indexOf.call(tr, parent.parentNode) - 1].children[parent.cellIndex].style.background = '#a58fbc';
+        tr[Array.prototype.indexOf.call(tr, parent.parentNode) - 1].children[parent.cellIndex].style.background = '#a17a7a';
 
       parent.style.background = 'rosybrown';
     }
